b15dd84a8817d0dd002ff69085b4972577a48f62
Muhamad Dimas Saputra <dimassaputrq96@gmail.com>
2026-09-22 12:30:39 +0700

    baru

--- src/main/java/com/Rumus/RumusController.java
+++ src/main/java/com/Rumus/RumusController.java
@@ -3,16 +3,25 @@ package com.Rumus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RumusController {
 
     @GetMapping("/rumus")
-    public String index(Model model) {
-        model.addAttribute("luas", 50);
-        model.addAttribute("keliling", 30);
-        model.addAttribute("isPersegi", false);
-        
-        return "Rumus"; // Memanggil /WEB-INF/jsp/Rumus.jsp
+    public String hitung(
+            @RequestParam(name = "panjang", required = false) Integer panjang,
+            @RequestParam(name = "lebar", required = false) Integer lebar,
+            Model model) {
+
+        if (panjang != null && lebar != null) {
+            boolean isValid = Rumus.validatePersegi(panjang, lebar);
+
+            model.addAttribute("panjang", panjang);
+            model.addAttribute("lebar", lebar);
+            model.addAttribute("isValid", isValid);
+        }
+
+        return "Rumus";
     }
 }
